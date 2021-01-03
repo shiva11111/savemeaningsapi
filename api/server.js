402e8b80b122cf8eef6f44a08134c8b3be444f28
shiva11111 <samwinchester12130@gmail.com>
2021-01-03 21:36:13 +0530

    first build

--- api/server.js
+++ api/server.js
@@ -2,8 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const PORT = 4000;
+const path = require("path");
+const serveStatic = require("serve-static");
+
+const PORT = process.env.PORT || 4000;
+
 const app = express();
+app.use(serveStatic(path.join(__dirname, "dist")));
+
 const Router = require("./meaningRouter");
 const config = require("./config");
 
